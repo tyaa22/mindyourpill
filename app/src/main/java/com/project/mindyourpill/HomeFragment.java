931db237d51abd612ef0f2b523c8704b11daa164
cyintia <meteorite01@outlook.com>
2024-06-06 18:23:01 +0700

    First commit

---
+++ app/src/main/java/com/project/mindyourpill/HomeFragment.java
@@ -0,0 +1,46 @@
+package com.project.mindyourpill;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import com.project.mindyourpill.databinding.HomeFragmentBinding;
+
+public class HomeFragment extends Fragment {
+
+    private HomeFragmentBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = HomeFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.addButtonImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(HomeFragment.this)
+                        .navigate(R.id.action_HomeFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
